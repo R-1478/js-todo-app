d72f2c9f6dcd448db47e0525215bc7f615bcc5ae
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2023-10-18 14:43:27 +0300

    Restyled the entire application

--- js/script.js
+++ js/script.js
@@ -1,16 +1,42 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  let form = document.querySelector('form');
+  let todoForm = document.querySelector('form');
+  let todoList = document.querySelector('.todo-list');
+
+  // GET - Read
+  fetch('http://127.0.0.1:3000/todos')
+    .then(res => res.json())
+    .then(todos => {
+      todos.forEach(todo => {
+        let listItem = document.createElement('li');
+        listItem.innerText = todo.title;
+        todoList.appendChild(listItem);
+      });
+    });
+
   
-  form.addEventListener('submit', (e) => {
+  todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    let inputElem = document.getElementById('todo-text');
-    let todoList = document.getElementById('todo-list');
+    let inputElem = document.getElementById('input--add');
 
     let listItem = document.createElement('li');
     console.log(listItem)
     listItem.innerText = inputElem.value;
-    todoList.appendChild(listItem);
+    todoList.appendChild(listItem); // Add new todo item to the DOM
+    // POST - Create
+    // Add todo item to the database
+    fetch('http://127.0.0.1:3000/todos', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        title: inputElem.value,
+        completed: false
+      })
+    });
+
+    todoForm.reset(); // Clears the form fields
   })
 
 });
-
