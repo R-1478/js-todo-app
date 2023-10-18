bdeb5d15fc40b55da0d1d2e4176aea6928c9b5c6
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2023-10-18 16:31:10 +0300

    Added update functionality

--- js/script.js
+++ js/script.js
@@ -2,14 +2,35 @@ document.addEventListener('DOMContentLoaded', ()=>{
   let todoForm = document.querySelector('form');
   let todoList = document.querySelector('.todo-list');
 
+  const createListItem = (todoObj) => {
+    let listItem = document.createElement('li');
+    listItem.innerText = todoObj.title;
+    todoObj.completed ? listItem.style.textDecoration = "line-through" : listItem.style.textDecoration = "none";
+    todoList.appendChild(listItem);
+    // Add a click event listener to list item
+    listItem.addEventListener("click", () => {
+      listItem.style.textDecoration = "line-through";
+
+      fetch(`http://127.0.0.1:3000/todos/${todoObj.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          completed: true
+        })
+      })
+    });
+  }
+
   // GET - Read
   fetch('http://127.0.0.1:3000/todos')
     .then(res => res.json())
     .then(todos => {
       todos.forEach(todo => {
-        let listItem = document.createElement('li');
-        listItem.innerText = todo.title;
-        todoList.appendChild(listItem);
+        console.log(todo);
+        createListItem(todo);
       });
     });
 
@@ -18,10 +39,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     e.preventDefault();
     let inputElem = document.getElementById('input--add');
 
-    let listItem = document.createElement('li');
-    console.log(listItem)
-    listItem.innerText = inputElem.value;
-    todoList.appendChild(listItem); // Add new todo item to the DOM
+    createListItem(inputElem.value);
+  
     // POST - Create
     // Add todo item to the database
     fetch('http://127.0.0.1:3000/todos', {
@@ -34,7 +53,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
         title: inputElem.value,
         completed: false
       })
-    });
+    }).then(res => res.json())
+      .then(todo => createListItem(todo));
 
     todoForm.reset(); // Clears the form fields
   })
